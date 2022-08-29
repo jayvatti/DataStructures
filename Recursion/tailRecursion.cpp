17a20a87a6b3e3e184f2e800a91147d46ae2066c
Jay Vatti <sujay.vatti@gmail.com>
2022-08-29 12:42:52 -0500

    tailRecursion

---
+++ Recursion/tailRecursion.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+// TAIL RECURSION
+/*
+ * Tail Recursion -> recursions at the end of the function
+ * The value returned by the recursion isn't used anywhere.
+ * Tail Recursions can be easily converted to loops
+ *
+ * Tail Recursions and loops take the same time.
+ * Tail Recursions, Loops -> O(n) -> Time Complexity
+ *
+ * Tail Recursions -> O(n) -> Space Complexity
+ * Loops           -> O(1) -> Space Complexity
+ *
+ * Therefore, Loops are space efficient. Many Compilers try to convert Tail Recursions to loops.
+ */
+void func(int n){ //LOOPS
+    while(n > 0){
+        std::cout<<n;
+        n--;
+    }
+}
+
+void func1(int n){ // TAIL RECURSION
+    if(n>0){
+        std::cout<<n<<std::endl;
+        func1(n-1);
+    }
+}
+
+int main() {
+    func1(4);
+}
