9bea5ab653f50cd225ba44085a700e82061d86be
Jay Vatti <sujay.vatti@gmail.com>
2022-08-29 12:09:23 -0500

    Update basicRecursion.cpp

--- Recursion/basicRecursion.cpp
+++ Recursion/basicRecursion.cpp
@@ -5,21 +5,30 @@
  */
 // TRACING TREE RECURSION
 
-void func1(int n){ // RETURN PHASE or ASCENDING
+void func1(int n){ // RETURN PHASE or ASCENDING or TAIL RECURSION
     if(n>0){
         std::cout<< n; // Executes at calling time
         func1(n-1);
     }
-}; // OUTPUT: 3 2 1
+}; // OUTPUT: 4 3 2 1
 
-void func(int n){//CALLING PHASE or DESCENDING
+void func(int n){//CALLING PHASE or DESCENDING or NON-TAIL RECURSION
     if(n>0){
         func(n-1);
         std::cout<<n; // Executes at return time
     }
-}; // OUTPUT: 1 2 3
+}; // OUTPUT: 1 2 3 4
 
 int main() {
-    func1(3);
-    func(2);
+    func1(4);
+    func(4);
 }
+
+
+
+
+
+
+
+
+
