f1a3669e32f52524e76718d23741cea503b194d5
Jay Vatti <sujay.vatti@gmail.com>
2022-09-03 16:44:08 -0400

    Update staticVariables.cpp

--- Recursion/staticVariables.cpp
+++ Recursion/staticVariables.cpp
@@ -8,17 +8,17 @@ int func1(int n){ // RECURSION WITHOUT STATIC VARIABLES
     return 0;
 };
 
-int func(int n){ // RECURSION WITH STATIC VARIABLE
+int func(int n){ // RECURSION WITH STATIC VARIABLE //SQUARING THE NUMBER
     static int x = 0;
     if(n>0){
         x++;
-        return func1(n-1) + x;
+        return func(n-1) + x;
     }
     return 0;
 }
 int main() {
-    std::cout<<func1(4)<<std::endl;
+    std::cout<<func1(5)<<std::endl;
 
-    std::cout<<func(4)<<std::endl;// Since func has a static variable, it acts as a global variable.
+    std::cout<<func(11)<<std::endl;// Since func has a static variable, it acts as a global variable.
     // You can use a global variable too (Outside func, int x = 0;)
 }
