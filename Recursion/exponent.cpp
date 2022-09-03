1f3c271ba942525b65a84903d4c596b80cfba720
Jay Vatti <sujay.vatti@gmail.com>
2022-09-03 17:09:51 -0400

    exponent

---
+++ Recursion/exponent.cpp
@@ -0,0 +1,44 @@
+//
+// Created by Jay Vatti on 8/29/22.
+#include <iostream>
+
+/*EXPONENTS
+ * 2^4 = 2 x 2 x 2 x 2
+ * exponent(m,n) = m * exponent(m,n-1).
+ * If n == 0; return 1
+ *
+ *ALTERNATE METHOD
+ * 2^8 = (2^2)^4 - Computing time is halved
+ * 2^9 = 2 * (2^2)^4
+ * */
+int exponent(int m,int n){
+    if(n==0){
+        return 1;
+    }
+    else{
+        return m* exponent(m,n-1); // TIME COMPLEXITY -
+    }
+}
+int power(int m,int n){
+    if(n==0){
+        return 1;
+    }
+    if(n%2 == 0){
+        return power(m*m,n/2);
+    }
+    else{
+        return m* power(m*m,(n-1)/2);
+    }
+}
+int main() {
+    std::cout<<power(2,9)<<std::endl;
+}
+//FLOWCHART OF power(2,9)
+/*
+ * power(2,9)
+ * 2 * power(2*2, (9-1)/2 = 4)
+ *     power(2*2, (9-1)/2 = 4) = power(2^2 * 2^2, 2)
+ *                               power(2^2 * 2^2, 2) = power((2^2 * 2^2) * (2^2 * 2^2) , 1)
+ *                                                     power(2^8 , 1) = 2^8 * power(2^8 * 2^8,0)   //ODD
+ *                                                                            power(2^16,0) = 1
+ */
